3aa2d10fbf16e64685df401053e75b12955c7487
Hadisa Syed <hadisaasyed@gmail.com>
2026-09-18 15:45:49 +0500

    replacing anchor tag with Link tag

--- Frontend/src/Components/Hero.jsx
+++ Frontend/src/Components/Hero.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Hero() {
     return (
         <section className="bg-slate-50">
@@ -22,12 +24,13 @@ function Hero() {
                     </p>
 
                     <div className="mt-8 flex flex-wrap gap-4">
-                        <a
+                        {/* <a
                             href="/calculator"
                             className="rounded-lg bg-blue-600 px-6 py-3 font-semibold text-white transition hover:bg-blue-700"
                         >
                             Start Estimator →
-                        </a>
+                        </a> */}
+                        <Link to="/calculator" className="rounded-lg bg-blue-600 px-6 py-3 font-semibold text-white transition hover:bg-blue-700">Start Estimator →</Link>
 
                         <a
                             href="#how-it-works"
